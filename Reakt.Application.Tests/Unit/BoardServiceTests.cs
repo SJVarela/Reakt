7f4a231fa873fe8cb6e3f6c31c1f67972d28b9f2
HEXACTA\svarela <svarela@hexacta.com>
2020-10-01 17:42:48 -0300

    Fixed test setups

--- Reakt.Application.Tests/Unit/BoardServiceTests.cs
+++ Reakt.Application.Tests/Unit/BoardServiceTests.cs
@@ -19,7 +19,7 @@ using Reakt.Server.MapperConfig;
 
 namespace Reakt.Application.Tests.Unit
 {
-    //test change
+    [TestFixture]
     class BoardServiceTests
     {
         private ReaktDbContext _context;
@@ -35,14 +35,16 @@ namespace Reakt.Application.Tests.Unit
                 Description = "Test desc"
             }
         };
-
-        [SetUp]
+        
+        [OneTimeSetUp]
         public void Setup()
         {
             //setup inmemorydb
             var x = new DbContextOptionsBuilder<ReaktDbContext>();
             x.UseInMemoryDatabase("UtDb");
             _context = new ReaktDbContext(x.Options);
+            _context.Boards.AddRange(_mockData);
+            _context.SaveChanges();
 
             _mapper = new Mapper(new MapperConfiguration(conf => conf.AddProfile(new BoardProfile())));
             _boardService = new BoardService(_context, _mapper);
@@ -52,27 +54,23 @@ namespace Reakt.Application.Tests.Unit
         public void Get_Should_Return_Results()
         {
             //Arrange
-            _context.Boards.AddRange(_mockData);
-            _context.SaveChanges();
-
+            var expected = _mapper.Map<List<Domain.Models.Board>>(_mockData);
             //Act
             var result = _boardService.Get();
 
             //Arrange
-            result.Should().BeEquivalentTo(_mapper.Map<List<Domain.Models.Board>>(_mockData));
+            result.Should().BeEquivalentTo(expected);
         }
         [Test]
         public void Get_by_Id_Should_Return_Results()
         {
             //Arrange
-            _context.Boards.AddRange(_mockData);
-            _context.SaveChanges();
-
+            var expected = _mapper.Map<Domain.Models.Board>(_mockData.First(b => b.Id == 1));
             //Act
             var result = _boardService.Get(1);
 
             //Arrange            
-            result.Should().BeEquivalentTo(_mapper.Map<Domain.Models.Board>(_mockData.First(b => b.Id == 1)));
+            result.Should().BeEquivalentTo(expected);
         }
     }
 }
