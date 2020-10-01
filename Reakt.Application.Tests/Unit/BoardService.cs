bed8f76fd14e77714ceb67a68dd866bacb950dfc
HEXACTA\svarela <svarela@hexacta.com>
2020-10-01 15:38:00 -0300

    Adding tests project

---
+++ Reakt.Application.Tests/Unit/BoardService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Reakt.Application.Tests.Unit
+{
+    class BoardService
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Get_Should_Return_Results()
+        {
+            //Arrange            
+            Assert.Pass();
+        }
+    }
+}
+
