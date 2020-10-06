d90850fa3a40738285d47cb9e768bae1915f039a
vicheroy <mnicolas.quintana@gmail.com>
2020-10-06 15:30:36 -0300

    Removed hidden auditable props

--- Reakt.Server/Models/Post.cs
+++ Reakt.Server/Models/Post.cs
@@ -10,33 +10,23 @@ namespace Reakt.Server.Models
     public class Post : AuditableEntity
     {
         /// <summary>
-        /// Post Title
+        /// Post comments
         /// </summary>
-        [Required]
-        [MaxLength(100)]
-        [MinLength(1)]
-        public string Title { get; set; }
+        public List<Comment> Comments { get; set; }
+
         /// <summary>
         /// Post description
         /// </summary>
         [MaxLength(4000)]
         [MinLength(1)]
         public string Description { get; set; }
-        /// Date the post was created
-        /// </summary>
-        public DateTime CreatedAt { get; private set; }
+
         /// <summary>
-        /// Date the post was last updated
+        /// Post Title
         /// </summary>
-        public DateTime? UpdatedAt { get; private set; }
-        /// <summary>
-        /// Date the post was deleted
-        /// </summary>
-        public DateTime? DeletedAt { get; private set; }
-        /// <summary>
-        /// Post comments
-        /// </summary>
-        public List<Comment> Comments { get; set; }
-       
+        [Required]
+        [MaxLength(100)]
+        [MinLength(1)]
+        public string Title { get; set; }
     }
 }
