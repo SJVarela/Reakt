731893995b4155805ababdb254b23905f70dbd25
SJVarela <s.javiervarela@gmail.com>
2020-10-14 12:04:43 -0300

    Removed CommentService from controller through mediatr

---
+++ Reakt.Application/Comments/Commands/Update/UpdateCommentCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Reakt.Domain.Models;
+
+namespace Reakt.Application.Comments.Commands.Update
+{
+    public class UpdateCommentCommand : IRequest<Comment>
+    {
+        public Comment Comment { get; set; }
+    }
+}
---
+++ Reakt.Application/Comments/Commands/Update/UpdateCommentCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Reakt.Application.Comments.Commands.Update;
+using Reakt.Application.Contracts.Interfaces;
+using Reakt.Domain.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Reakt.Application.Comments.Update
+{
+    public class UpdateCommentCommandHandler : IRequestHandler<UpdateCommentCommand, Comment>
+    {
+        private readonly ICommentService _commentService;
+
+        public UpdateCommentCommandHandler(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        public Task<Comment> Handle(UpdateCommentCommand request, CancellationToken cancellationToken)
+        {
+            return _commentService.UpdateAsync(request.Comment);
+        }
+    }
+}
