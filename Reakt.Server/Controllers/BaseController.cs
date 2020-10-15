8ad91af6a626a2b5619a24a1b88d13afa4ee7c23
SJVarela <s.javiervarela@gmail.com>
2020-10-15 11:46:43 -0300

    Moved to a base controller to unify functionality

---
+++ Reakt.Server/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Reakt.Server.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public abstract class BaseController : ControllerBase
+    {
+        protected ILogger Logger { get; }
+        protected IMapper Mapper { get; }
+        protected IMediator Mediator { get; }
+
+        public BaseController(IMediator mediator, IMapper mapper, ILogger logger)
+        {
+            Mapper = mapper;
+            Logger = logger;
+            Mediator = mediator;
+        }
+    }
+}
