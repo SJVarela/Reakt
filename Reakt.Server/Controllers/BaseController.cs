2631cba183f8e65c4971cda9fa30b3725516a9e0
SJVarela <s.javiervarela@gmail.com>
2020-10-15 15:53:12 -0300

    Move BoardsController to BaseController implementation

--- Reakt.Server/Controllers/BaseController.cs
+++ Reakt.Server/Controllers/BaseController.cs
@@ -5,10 +5,17 @@ using Microsoft.Extensions.Logging;
 
 namespace Reakt.Server.Controllers
 {
+    /// <summary>
+    /// </summary>
     [Route("api")]
     [ApiController]
     public abstract class BaseController : ControllerBase
     {
+        /// <summary>
+        /// </summary>
+        /// <param name="mediator"></param>
+        /// <param name="mapper"></param>
+        /// <param name="logger"></param>
         protected BaseController(IMediator mediator, IMapper mapper, ILogger logger)
         {
             Mapper = mapper;
@@ -16,8 +23,16 @@ namespace Reakt.Server.Controllers
             Mediator = mediator;
         }
 
+        /// <summary>
+        /// </summary>
         protected ILogger Logger { get; }
+
+        /// <summary>
+        /// </summary>
         protected IMapper Mapper { get; }
+
+        /// <summary>
+        /// </summary>
         protected IMediator Mediator { get; }
     }
 }
